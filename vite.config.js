bd90176132f60e45805773a77e82e3f8db7273b5
Mike <micdsn@gmail.com>
2026-05-21 16:11:33 -0500

    fixing to launch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/play/',
-  build: {
-    outDir: 'dist/play',
-  },
+  build: { outDir: 'dist' },
 })
